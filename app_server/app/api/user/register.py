8fb1e178d09dcba059e7b50d54bf57ddb9c39048
zldevil2011 <929034478@qq.com>
2015-10-12 21:54:50 +0800

    fix bug for user register_api

--- app_server/app/api/user/register.py
+++ app_server/app/api/user/register.py
@@ -7,6 +7,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.core.exceptions import ObjectDoesNotExist
 from app.serializers import RegisterSerializer
 import datetime
+import random
+seed = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789_-'
+
 class Register(APIView):
     @csrf_exempt
     def post(self, request, format=None):
@@ -17,17 +20,26 @@ class Register(APIView):
                 admin_user = User.objects.get(email=serializer.data['email'])
                 return Response({"status": "user exist"},status=status.HTTP_400_BAD_REQUEST)
             except ObjectDoesNotExist:
+                username = ""
+                num = random.randint(4, 10)
+                while True:
+                    username = ""
+                    for i in range(num):
+                        username += random.choice(seed)
+                    try:
+                        user = AppUser.objects.get(username=username)
+                    except AppUser.DoesNotExist:
+                        break
                 password = serializer.data['password']
                 email = serializer.data['email']
-                username = "test_user"
-                # admin_user = User.objects.create_user(username=username, password=password,
-                #                                       last_login=datetime.datetime.now(), email=email)
-                # admin_user.save()
-                # user = AppUser()
-                # user.username = username
-                # user.password = password
-                # user.user = admin_user
-                # user.email = email
-                # user.save()
+                admin_user = User.objects.create_user(username=username, password=password,
+                                                      last_login=datetime.datetime.now(), email=email)
+                admin_user.save()
+                user = AppUser()
+                user.username = username
+                user.password = password
+                user.user = admin_user
+                user.email = email
+                user.save()
                 return Response(status=status.HTTP_201_CREATED)
         return Response({"status": "format error"}, status=status.HTTP_400_BAD_REQUEST)
