cd10f5e239401ff0243774274fbcb8d6e6340113
zldevil2011 <929034478@qq.com>
2015-10-12 22:43:42 +0800

    add models

--- app_server/app/admin.py
+++ app_server/app/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from app.models import AppUser
+from app.models import AppUser, Air, News
 
 
 class AppUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'user')
 
+
+class AirAdmin(admin.ModelAdmin):
+    list_display = ('air_id', 'pm25')
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('news_id', 'news_title')
+
 admin.site.register(AppUser, AppUserAdmin)
+admin.site.register(Air, AirAdmin)
+admin.site.register(News, NewsAdmin)
 # Register your models here.
--- app_server/app/models.py
+++ app_server/app/models.py
@@ -13,4 +13,31 @@ class AppUser(models.Model):
         return str(self.username)
 
 
+class Air(models.Model):
+    air_id = models.IntegerField()
+    pm25 = models.FloatField()
+    cloud = models.FloatField()
+    rain = models.FloatField()
+    ziwai = models.FloatField()
+    guang = models.FloatField()
+    clouddir = models.CharField(max_length=200)
+    time = models.DateTimeField()
+
+    def __unicode__(self):
+        return str(self.air_id)
+
+
+class News(models.Model):
+    news_id = models.AutoField(primary_key=True)
+    news_title = models.CharField(max_length=200)
+    news_type = models.IntegerField()
+    news_content = models.CharField(max_length=10000)
+    news_update_time = models.CharField(max_length=200)
+    news_priori = models.IntegerField(default=0)
+    news_readtime = models.IntegerField(default=0)
+    news_author = models.ForeignKey(AppUser, related_name='news', null=True)
+
+    def __unicode__(self):
+        return str(self.news_id)
+
 # Create your models here.
